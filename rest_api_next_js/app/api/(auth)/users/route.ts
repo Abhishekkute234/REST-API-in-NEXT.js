3e07f71cc497251f92dbc4687a6b11a31559620c
Abhishekkute234 <kuteabhishek2004@gmail.com>
2024-12-26 09:07:04 +0530

    adding the post request

--- rest_api_next_js/app/api/(auth)/users/route.ts
+++ rest_api_next_js/app/api/(auth)/users/route.ts
@@ -21,3 +21,24 @@ export const GET = async () => {
     );
   }
 };
+
+
+// postman API to post the data in the database 
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
+    await connect();
+
+    const newUser = new User(body);
+    await newUser.save();
+
+    return new NextResponse(JSON.stringify({ message: "User is created ", user: newUser }),
+      { status: 200 }
+
+    );
+  } catch (error: any) {
+    return new NextResponse("Error in creating user" + error.message, { status: 500 });
+
+  }
+
+}
